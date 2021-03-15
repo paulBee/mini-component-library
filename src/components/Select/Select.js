0dcbbb511d956b77c40e6ce01d57f8bbc4247bd7
Paul B <paul.beecroft@gmail.com>
2021-03-15 23:04:23 +0000

    Select component
    - struggled with the outline as I cant see how to select the parent of the focused element without mirroring the state in React. Im sure the video will reveal all!

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
+import VisuallyHidden from "../VisuallyHidden";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
+  const [hasFocus, setFocus] = useState(false)
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+  return <>
+      <Wrapper hasFocus={hasFocus}>
+        <VisuallyHidden><label for={label}>{label}</label></VisuallyHidden>
+        {displayedValue}
+        <Toggle id="chevron-down" size={24} />
+        <SelectInteraction
+            value={value}
+            id={label}
+            onChange={onChange}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            aria-labelled
+        >
+          {children}
+        </SelectInteraction>
+      </Wrapper>
+  </>;
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 1rem;
+  padding: 12px 52px 12px 16px;
+  width: max-content;
+  border-radius: 8px;
+  
+  outline: ${p => p.hasFocus ? "5px auto #4374CB": ""};
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const Toggle = styled(Icon)`
+  position: absolute;
+  top: 8px;
+  right: 10px;
+`
+
+const SelectInteraction = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
 
 export default Select;
