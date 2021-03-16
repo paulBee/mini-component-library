b3e2adf0e9ebcb189d7d35bbfe2a80f434c1e2d9
Paul B <paul.beecroft@gmail.com>
2021-03-16 20:37:25 +0000

    Icon Input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 import { COLORS } from '../../constants';
 
@@ -11,9 +11,75 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...inputProps
 }) => {
-  return 'TODO';
+  return (
+      <Wrapper width={width + "px"} size={size}>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        <IconWrapper>
+          <Icon id={icon} size={ICON_SIZES[size]} />
+        </IconWrapper>
+        <TextInput {...inputProps} />
+      </Wrapper>
+  )
 };
+
+const ICON_SIZES = {
+  small: 16,
+  large: 24
+}
+
+const SIZES = {
+  small: css`
+    --fontSize: ${14/16}rem;
+    --iconSize: ${ICON_SIZES.small}px;
+    --borderThickness: 1px;
+    --height: ${24/16}rem;
+  `,
+  large: css`
+    --fontSize: ${18/16}rem;
+    --iconSize: ${ICON_SIZES.large}px;
+    --borderThickness: 2px;
+    --height: ${36/16}rem;
+  `
+}
+
+const Wrapper = styled.label`
+  position: relative;
+  display: block;
+  color: ${COLORS.gray700};
+  width: ${p => p.width};
+  
+  ${p => SIZES[p.size]}
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  margin: auto 0;
+  top: 0;
+  bottom: 0;
+  height: var(--iconSize);
+`
+
+const TextInput = styled.input`
+  padding-left: var(--height);
+  border: none;
+  border-bottom: var(--borderThickness) solid ${COLORS.black};
+  height: var(--height);
+  font-size: var(--fontSize);
+  font-weight: 700;
+  width: 100%;
+  outline-offset: 2px;
+  color: inherit;
+  
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`
 
 export default IconInput;
